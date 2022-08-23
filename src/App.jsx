3ec118d02c52b7936c8d84ca04b9069532880794
OkenHaha <keithellakpamoken@gmail.com>
2022-08-23 18:40:59 +0530

    Done with homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,68 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Top from './Components/Top_devs/Top'
 import Dev_Card from './Components/DevCard/Dev_Card'
+import Footer from './Components/Footer/Footer'
+import { faker } from '@faker-js/faker'
+
+
+const list_logo = faker.image.avatar()
+
 function App() {
   return (
     <div className="App">
-      <Dev_Card/>
+      <Navbar/>
+      <Hero/>
+      <Top/> 
+      <div className="companies">
+        <h1 className="title">20k Companies <span>Devhunt</span></h1>
+      </div>
+      <div className="company_list">
+        <div className="list_row">
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+        </div>
+        <div className="list_row">
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+        </div>
+        <div className="list_row">
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+        </div>
+        <div className="list_row">
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+          <div className="company_logo">
+            <img src={list_logo} alt=""/>
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.scss'
+import Logo from '../../assets/logo2.svg'
+const Footer = () => {
+	return (
+		<div className="footer_content">
+			<div className="logo">
+				<img src={Logo} alt=""/>
+			</div>
+  			<div className="sitemap">
+  				<div className="footer_title">Sitemap</div>
+  				<div>For Employers</div>
+  				<div>For Freelancers</div>
+  				<div>Guide</div>
+  				<div>Account</div>
+  			</div>
+  			<div className="sitemap">
+  				<div className="footer_title">Sitemap</div>
+  				<div>For Employers</div>
+  				<div>For Freelancers</div>
+  				<div>Guide</div>
+  				<div>Account</div>
+  			</div>
+  			<div className="sitemap">
+  				<div className="footer_title">Sitemap</div>
+  				<div>For Employers</div>
+  				<div>For Freelancers</div>
+  				<div>Guide</div>
+  				<div>Account</div>
+  			</div>
+  			<div className="sitemap">
+  				<div className="footer_title">Sitemap</div>
+  				<div>For Employers</div>
+  				<div>For Freelancers</div>
+  				<div>Guide</div>
+  				<div>Account</div>
+  			</div>
+
+		</div>
+	)
+}
+
+export default Footer
